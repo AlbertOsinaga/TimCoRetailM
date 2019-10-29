f2ceeef4035ba009e469ee57f933fc4778d4d1c0
Luis Alberto Osinaga <luis.osinaga@gmail.com>
2019-10-29 11:20:07 -0400

    Web Api with Identity package

--- TCRMDataManager/Properties/AssemblyInfo.cs
+++ TCRMDataManager/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("393e70f9-ecd6-4671-88e9-be2e3abb4655")]
+[assembly: Guid("e8576c07-cbc0-452e-8c73-d6bb0a0613a8")]
 
 // Version information for an assembly consists of the following four values:
 //
